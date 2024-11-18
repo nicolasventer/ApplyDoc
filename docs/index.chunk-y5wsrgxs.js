d387532d650190690d08a8f7f0b15d448dbae2eb
Nicolas VENTER <thedeal78@hotmail.fr>
2024-11-18 15:27:55 +0100

    fix export

---
+++ docs/index.chunk-y5wsrgxs.js
@@ -0,0 +1,5 @@
+import{F as g,x as B,y as h}from"./index.chunk-57m45w0w.js";import{ta as w,wa as A}from"./index.chunk-tm5t0tn2.js";var F=(b)=>{return(I)=>g(B,{fallback:!0,children:g(b,{...I},void 0,!1,void 0,this)},void 0,!1,void 0,this)},G=(b)=>(q)=>q[b],K=F(h(()=>import("./pages/Home.chunk-8g12efee.js").then(G("HomePage")))),M=F(h(()=>import("./components/app/DocumentCard.chunk-rqwcgd10.js").then(G("DocumentCard"))));
+export{K as a,M as b};
+
+//# debugId=61FE03B98DE8630164756E2164756E21
+//# sourceMappingURL=index.chunk-y5wsrgxs.js.map
