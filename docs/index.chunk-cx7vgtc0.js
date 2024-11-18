57cf150ebbd6b5219395bd4569cf969108da0233
Nicolas VENTER <thedeal78@hotmail.fr>
2024-11-18 15:05:02 +0100

    split + import/export

---
+++ docs/index.chunk-cx7vgtc0.js
@@ -0,0 +1,5 @@
+import{F as g,x as B,y as h}from"./index.chunk-8tqeqcr9.js";import{sa as w,va as A}from"./index.chunk-q2p3fra1.js";var F=(b)=>{return(I)=>g(B,{fallback:!0,children:g(b,{...I},void 0,!1,void 0,this)},void 0,!1,void 0,this)},G=(b)=>(q)=>q[b],K=F(h(()=>import("./pages/Home.chunk-m6m8xzya.js").then(G("HomePage")))),M=F(h(()=>import("./components/app/DocumentCard.chunk-vp8b47nb.js").then(G("DocumentCard"))));
+export{K as a,M as b};
+
+//# debugId=20BF91E41DA6E73464756E2164756E21
+//# sourceMappingURL=index.chunk-cx7vgtc0.js.map
