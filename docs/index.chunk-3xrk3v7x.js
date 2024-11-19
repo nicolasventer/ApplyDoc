4747829d3b9bf585e9e4372e25644cb9324c0596
Nicolas VENTER <thedeal78@hotmail.fr>
2024-11-19 12:49:50 +0100

    pretty scrollbar

---
+++ docs/index.chunk-3xrk3v7x.js
@@ -0,0 +1,5 @@
+import{A as h,H as g,z as B}from"./index.chunk-xb1raj60.js";import{va as w,ya as A}from"./index.chunk-3fkxm2sb.js";var F=(b)=>{return(I)=>g(B,{fallback:!0,children:g(b,{...I},void 0,!1,void 0,this)},void 0,!1,void 0,this)},G=(b)=>(q)=>q[b],K=F(h(()=>import("./pages/Home.chunk-0rkyzq4f.js").then(G("HomePage")))),M=F(h(()=>import("./components/app/DocumentCard.chunk-cqkn387w.js").then(G("DocumentCard"))));
+export{K as a,M as b};
+
+//# debugId=C418228877703C5364756E2164756E21
+//# sourceMappingURL=index.chunk-3xrk3v7x.js.map
